8df516a2811a88ca22539a6b93a005b5b27d764a
Cossta07 <samuellinker46@gmail.com>
2024-03-13 16:47:01 -0400

    questão 3 feita

---
+++ questão-3.js
@@ -0,0 +1,8 @@
+/*
+Questão 3: Como você pode concatenar duas strings em JavaScript?
+*/
+
+let nome = "Samuka";
+let sobrenome = "costa!";
+let resultado = nome +" " + sobrenome;
+console.log(resultado);
