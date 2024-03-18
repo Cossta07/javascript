6ca6802d62e6fa29d8861d33a67478f9ad1df7ae
Cossta07 <samuellinker46@gmail.com>
2024-03-18 15:41:35 -0400

    Comprimento de String em JS

---
+++ questao15.js
@@ -0,0 +1,12 @@
+/*
+Como você pode obter o comprimento de uma string em 
+JavaScript?
+Resposta:
+Uma das maneiras mais simples de obter o comprimento de uma string em JavaScript é utilizando a propriedade length. 
+Essa propriedade retorna o número de caracteres da string, incluindo espaços em branco e caracteres especiais.
+*/
+let minhaString = "Olá, mundo!";
+
+let comprimento = minhaString.length;
+ 
+ console.log(comprimento); // Output: 12
