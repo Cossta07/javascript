4c03574fdebff23224ac92ce774db4e152d6e504
Cossta07 <samuellinker46@gmail.com>
2024-03-18 16:26:16 -0400

    loop while

---
+++ questap23.js
@@ -0,0 +1,10 @@
+/*
+
+Como você pode criar um loop while em JavaScript?
+*/
+let contador = 1;
+
+			while (contador <= 5) {
+  				console.log("Contador: " + contador);
+  				contador++;
+            }
