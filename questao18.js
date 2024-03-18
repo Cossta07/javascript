2b7091938494d222033c4b07a5f64e31f330384e
Cossta07 <samuellinker46@gmail.com>
2024-03-18 15:53:02 -0400

    Acessar valor de um objeto

---
+++ questao18.js
@@ -0,0 +1,16 @@
+/*
+Como você pode acessar o valor de uma propriedade de um 
+objeto em JavaScript?
+Resposta:
+propriedade ponto .
+*/
+
+const myProfile = {
+    title: "@samuellinker46@gmail.com",
+    url: "https://twitter.com/costa",
+    tweets: 9,
+    followers: 4,
+    following: 6
+  };
+console.log(myProfile.title); // @samuelliker46@gmail.com
+console.log(myProfile.tweets); // 9
