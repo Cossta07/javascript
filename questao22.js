4492f2d17c9afdada3e6ec9215ea8039835b196b
Cossta07 <samuellinker46@gmail.com>
2024-03-18 16:19:15 -0400

    variavel nula em js

---
+++ questao22.js
@@ -0,0 +1,9 @@
+/*
+
+Como você pode verificar se uma variável é nula em JavaScript?
+Resposta:
+Para verificar se um valor é nulo, podemos comparar com null, utilizando o operador ===
+*/
+if(x === null) {
+    console.log("X é null!");
+}
