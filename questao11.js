911031e8cf8bd6fb6fc37bc3534dd9fea3b965fd
Cossta07 <samuellinker46@gmail.com>
2024-03-18 15:28:56 -0400

    Diferença de let, const e var

---
+++ questao11.js
@@ -0,0 +1,12 @@
+/*
+Qual é a diferença entre let, const e var em JavaScript?
+Resposta:
+A principal diferença entre var e let é o escopo. 
+Variáveis declaradas com var têm escopo global ou de função, 
+enquanto variáveis declaradas com let têm escopo de bloco.
+
+A principal diferença entre var e const é a mutabilidade. 
+Variáveis declaradas com var podem ser atualizadas, 
+enquanto variáveis declaradas com const não podem.
+*/
+
