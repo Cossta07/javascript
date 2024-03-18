d1928b3fe40746b31cc4f42734ed871a3b552df7
Cossta07 <samuellinker46@gmail.com>
2024-03-18 16:36:29 -0400

    operador ternário em js

---
+++ questao26.js
@@ -0,0 +1,12 @@
+/*
+
+Como você pode usar o operador ternário em JavaScript?
+Resposta:
+A sintaxe geral é a seguinte:
+condicao ? expressao_se_verdadeira : expressao_se_falsa
+*/
+
+let idade = 20;
+let status = (idade >= 18) ? "Adulto" : "Menor de idade";
+
+console.log(status); // Saída: "Adulto"
