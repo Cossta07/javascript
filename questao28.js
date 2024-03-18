93d26dbf0d9c9fce320192bcb904a6f40d233d17
Cossta07 <samuellinker46@gmail.com>
2024-03-18 16:45:56 -0400

    String em número inteiro

---
+++ questao28.js
@@ -0,0 +1,12 @@
+/*
+
+Como você pode converter uma string em um número inteiro em 
+JavaScript?
+Resposta:
+Utilizando a Função parseInt() para Converter uma String em um Número Inteiro em JavaScript.
+ A função parseInt() é amplamente utilizada em JavaScript para converter uma string em um número inteiro.
+*/
+let numeroString = "42";
+let numero = parseInt(numeroString);
+
+console.log(numero); // 42
