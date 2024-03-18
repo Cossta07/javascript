178f8286037cd0b27ac7640354e02e116a249b20
Cossta07 <samuellinker46@gmail.com>
2024-03-18 16:49:03 -0400

    número em string

---
+++ questao29.js
@@ -0,0 +1,11 @@
+/*
+
+Como você pode converter um número em uma string em 
+JavaScript?
+Resposta:
+O método toString() é uma forma comum e simples de converter um número em uma string em JavaScript. 
+Podemos aplicar esse método diretamente ao número que queremos converter, usando a sintaxe “numero.toString()”
+*/
+let numero = 123;
+let stringNumero = numero.toString();
+console.log(stringNumero); // Saída: "123"
