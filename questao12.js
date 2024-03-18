9b6d6a6e1a63f8aa68fa84c83ae67d88985ec9d7
Cossta07 <samuellinker46@gmail.com>
2024-03-18 15:31:33 -0400

    iterar array usando loop

---
+++ questao12.js
@@ -0,0 +1,13 @@
+/*
+Como você pode iterar por meio de um array em JavaScript 
+usando um loop for?
+Resposta:
+Em JavaScript, existem várias formas de iterar um array.
+ A maneira mais simples é usando um loop for. 
+ O loop for permite percorrer cada elemento do array, um por um, 
+ executando um bloco de código para cada elemento.
+*/
+const array = [1, 2, 3, 4, 5];
+    for (let i = 0; i < array.length; i++) {
+      console.log(array[i]);
+    }
