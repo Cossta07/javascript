30d54291829b5f6e04e66dd674567a6d742cbe9a
Cossta07 <samuellinker46@gmail.com>
2024-03-18 15:48:06 -0400

    Adicionar propriedade a um objeto em JS

---
+++ questao17.js
@@ -0,0 +1,10 @@
+/*
+Como você pode adicionar uma propriedade a um objeto em 
+JavaScript?
+Resposta:
+notação de colchetes
+*/
+const person = {};
+const propertyName = 'name';
+person[propertyName] = 'John';
+console.log(person[propertyName]); // Output: 'John'
