f52c16a84cfeeb23e386cd9f5063e34a529c2aa0
Cossta07 <samuellinker46@gmail.com>
2024-03-18 16:29:32 -0400

    evento de clique html em js

---
+++ questao24.js
@@ -0,0 +1,12 @@
+/*
+
+Como você pode adicionar um evento de clique a um elemento 
+HTML em JavaScript?
+*/
+
+<button class="button" onclick="myFunction()">
+  OK!
+</button>
+function myFunction() {
+    console.log('Clicou no botão "OK!"');
+  }
