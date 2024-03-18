28bd992c3169ff47cf573e6be71484080d6897c7
Cossta07 <samuellinker46@gmail.com>
2024-03-18 16:32:52 -0400

    Declarar função anonima em js

---
+++ questao25.js
@@ -0,0 +1,13 @@
+/*
+
+Como você pode declarar uma função anônima em JavaScript?
+Resposta:
+Atribuindo uma função anônima a uma variável:
+*/
+
+let minhaFuncao = function() {
+    console.log("Esta é uma função anônima.");
+};
+
+// Chamando a função anônima
+minhaFuncao();
