5c048eedf463059a5ebdeca541c129d5d415c0a4
Cossta07 <samuellinker46@gmail.com>
2024-03-18 16:00:33 -0400

    chamar função de retorno

---
+++ questao20.js
@@ -0,0 +1,12 @@
+/*
+Como você pode chamar uma função de retorno de chamada em 
+JavaScript?
+*/
+
+// Definindo a função de retorno de chamada
+function minhaFuncaoDeRetorno() {
+    console.log("A função de retorno de chamada foi chamada!");
+}
+
+// Chamando a função de retorno de chamada
+minhaFuncaoDeRetorno();
